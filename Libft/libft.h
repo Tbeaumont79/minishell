5694acd1204f57d9843decbfd6abea39f366553f
MACBOOK <macbook@MacBook-Pro-de-MACBOOK.home>
2020-03-19 12:01:06 +0100

    ez

--- Libft/libft.h
+++ Libft/libft.h
@@ -44,6 +44,7 @@ size_t				ft_strlcat(char *dest, const char *src, size_t size);
 size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 size_t				ft_strlen(const char *str);
 int					ft_strncmp(const char *s1, const char *s2, size_t i);
+int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strnstr(const char *haystack, const char *needle,
 					size_t len);
 char				*ft_strrchr(const char *str, int c);
